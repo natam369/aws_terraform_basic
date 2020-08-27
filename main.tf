4f39a01d7316c2f77b56ffe5d72644b4d78d345b
natam369 <58681656+natam369@users.noreply.github.com>
2020-08-27 19:42:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,107 @@
-aws_access_key = "XXXXXXXXXXXXX"
-aws_secret_key = "XXXXXXXXXXXXXXXXXXXXX"
-aws_region = "us-east-1"
-vpc_cidr = "10.1.0.0/16"
-public_subnet1_cidr = "10.1.1.0/24"
-public_subnet2_cidr = "10.1.2.0/24"
-public_subnet3_cidr = "10.1.3.0/24"
-private_subnet_cidr = "10.1.20.0/24"
-vpc_name = "terraform-aws-testing"
-IGW_name = "terraform-aws-igw"
-public_subnet1_name = "Terraform_Public_Subnet1-testing"
-public_subnet2_name = "Terraform_Public_Subnet2-testing"
-public_subnet3_name = "Terraform_Public_Subnet3-testing"
-private_subnet_name = "Terraform_Private_Subnet-testing"
-Main_Routing_Table = "Terraform_Main_table-testing"
-key_name = "LaptopKey"
-environment = "dev"
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+        Owner = "Sree"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+        tags = {
+        Name = "${var.IGW_name}"
+    }
+}
+
+resource "aws_subnet" "subnet1-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet1_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "${var.public_subnet1_name}"
+    }
+}
+
+resource "aws_subnet" "subnet2-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet2_cidr}"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "${var.public_subnet2_name}"
+    }
+}
+
+resource "aws_subnet" "subnet3-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet3_cidr}"
+    availability_zone = "us-east-1c"
+
+    tags = {
+        Name = "${var.public_subnet3_name}"
+    }
+
+}
+
+
+resource "aws_route_table" "terraform-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags = {
+        Name = "${var.Main_Routing_Table}"
+    }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    route_table_id = "${aws_route_table.terraform-public.id}"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "web-1" {
+    #ami = "ami-0bcc094591f354be2" 
+    ami = "${var.region_ami["${var.aws_region}"]}"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true
+    tags = {
+        Name = "Server-1"
+        Env = "Prod"
+        Owner = "lahari"
+       
+    }
+}
